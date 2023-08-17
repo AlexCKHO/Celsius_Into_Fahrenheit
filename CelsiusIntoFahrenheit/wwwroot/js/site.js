d7011ad7812004bdeabf77a4b8e9ecb5077a8386
Alex Ho <cho@spartaglobal.com>
2023-08-17 12:20:21 +0100

    JavaScript moved to site.js

--- CelsiusIntoFahrenheit/wwwroot/js/site.js
+++ CelsiusIntoFahrenheit/wwwroot/js/site.js
@@ -3,3 +3,40 @@
 
 // Write your JavaScript code.
 
+function setThermometerTemperature(celsiusTemp) {
+
+    let tempVal = document.getElementById("temp-val");
+    let tempFill = document.getElementById("temp-fill");
+    let tempF = nearest10th(CToF(celsiusTemp));
+
+    tempVal.setAttribute("data-c", celsiusTemp);
+    tempVal.setAttribute("data-f", tempF);
+
+    let scaleY = 0,
+        scaleYMax = 0.995;
+    let minTempC = -53,
+        maxTempC = 70;
+
+
+    scaleY += ((celsiusTemp - minTempC) / (Math.abs(minTempC) + maxTempC)) * 1.0907555;
+
+    if (scaleY > scaleYMax)
+        scaleY = scaleYMax;
+    else if (scaleY < 0)
+        scaleY = 0;
+
+    tempFill.style.transform = `scaleY(${scaleY})`;
+    tempVal.title = `${celsiusTemp}°C, ${tempF}°F`;
+}
+
+
+
+// Convert Celsius to Fahrenheit
+function CToF(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+// Round to the nearest 10th
+function nearest10th(n) {
+    return Math.round(n * 10) / 10;
+}
